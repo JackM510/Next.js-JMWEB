97caf3a54f45b4113b29c1259f790cf42b11288c
Jack <jackcmarshall@hotmail.com>
2026-05-28 09:22:48 +1000

    added ContactSection template structure

---
+++ src/app/components/sections/ContactSection.tsx
@@ -0,0 +1,11 @@
+import SectionHeading from "../ui/SectionHeading"
+
+export default function ContactSection() {
+    return (
+        <section id="contact" className="bg-blue-300">
+            <div className="content-container">
+                <SectionHeading color="text-[var(--dark-azure)]">Contact Details</SectionHeading>
+            </div>
+        </section>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "./components/sections/HeroSection";
 import AboutSection from "./components/sections/AboutSection";
 import ServicesSection from "./components/sections/ServicesSection";
+import ContactSection from "./components/sections/ContactSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection/>
       <ServicesSection/>
+      <ContactSection />
     </main>
   );
 }
