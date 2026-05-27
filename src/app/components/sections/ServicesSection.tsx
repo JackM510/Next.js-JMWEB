0556deb0d02bf379865c9eb00266c1640a0cbcea
Jack <jackcmarshall@hotmail.com>
2026-05-28 08:26:26 +1000

    Added the services section component to the project

---
+++ src/app/components/sections/ServicesSection.tsx
@@ -0,0 +1,9 @@
+import SectionHeading from "../ui/SectionHeading"
+
+export default function ServicesSection() {
+    return (
+        <div id="services" className="content-container">
+            <SectionHeading>Explore Services</SectionHeading>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/sections/HeroSection";
 import AboutSection from "./components/sections/AboutSection";
+import ServicesSection from "./components/sections/ServicesSection";
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <AboutSection/>
+      <ServicesSection/>
     </main>
   );
 }
