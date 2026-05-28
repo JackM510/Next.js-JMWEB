536124e729133ed3f545eabfa73de23448f67259
Jack <jackcmarshall@hotmail.com>
2026-05-28 14:56:42 +1000

    added 3 ServiceCards to the section, testing/refining responsive behaviour

--- src/app/components/sections/ServicesSection.tsx
+++ src/app/components/sections/ServicesSection.tsx
@@ -1,10 +1,45 @@
 import SectionHeading from "../ui/SectionHeading"
+import InfoRow from "../ui/InfoRow"
+import ServiceCard from "../ui/ServiceCard";
 
 export default function ServicesSection() {
     return (
         <section id="services" className="bg-[#011a2e]">
             <div className="content-container">
                 <SectionHeading color="text-white">Explore Services</SectionHeading>
+                <InfoRow color="text-[var(--dark-azure)]" background="bg-[var(--ghost-white)]"
+                    title="Launch a Website"
+                    text="Start your online journey today. Answer 6 quick questions to receive tailored solutions for your project."
+                    imgSrc="/assets/images/service-offering.jpg"
+                    imgAlt="test"
+                    reverse
+                />
+                <hr className="mb-12"></hr>
+                <div className="grid grid-cos-1 md:grid-cols-2 lg:grid-cols-3 items-stretch gap-4">
+                    {/* Web Development */}
+                    <ServiceCard 
+                        id="web-development"
+                        icon="assets/icons/typescript.svg"
+                        title="Web Development"
+                        description="Build a website tailored to your needs or refine your existing site as your business grows."
+                    />
+                    {/* Web Development */}
+                    <ServiceCard 
+                        id="web-development"
+                        icon="assets/icons/typescript.svg"
+                        title="Lorem ipsum"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae
+  ullamcorper justo."
+                    />
+                    {/* Web Development */}
+                    <ServiceCard 
+                        id="web-development"
+                        icon="assets/icons/typescript.svg"
+                        title="Lorem ipsum"
+                        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae
+  ullamcorper justo."
+                    />
+                </div>
             </div>
         </section>
     );
