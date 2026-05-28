9aaf72c4828005c887f51c3285c168fa1f45d50c
Jack <jackcmarshall@hotmail.com>
2026-05-28 12:58:40 +1000

    added color and background props to InfoRow

--- src/app/components/ui/InfoRow.tsx
+++ src/app/components/ui/InfoRow.tsx
@@ -10,12 +10,12 @@ type InfoRowProps = {
 
 export default function InfoRow({ title, text, color, background, imgSrc, imgAlt, reverse }: InfoRowProps) {
     return (
-        <div className={`flex flex-col lg:flex-row ${reverse ? "lg:flex-row-reverse" : ""} mb-12 `}>
-            <div className="basis-full lg:basis-2/5">
-                <img className="w-full h-auto shadow-lg" src={imgSrc} alt={imgAlt} />
+        <div className={`flex flex-col lg:flex-row ${reverse ? "lg:flex-row-reverse" : ""} shadow-lg lg:shadow-none mb-12 gap-x-2`}>
+            <div className="basis-full lg:basis-2/5 shadow-none lg:shadow-lg">
+                <img className="w-full h-auto object-cover" src={imgSrc} alt={imgAlt} />
             </div>
-            <div className={`${color} ${background } basis-full lg:basis-3/5 px-4 py-6 lg:p-8`}>
-                <h2 className="text-3xl md:text-4xl font-bold mb-4 pb-2 border-b border-b-[1px] border-b-[var(--azure)]">{title}</h2>
+            <div className={`${color} ${background } basis-full lg:basis-3/5 px-4 py-6 lg:p-8 shadow-none lg:shadow-lg`}>
+                <h2 className="text-3xl lg:text-4xl font-bold mb-4 pb-2 border-b border-b-[1px] border-b-[var(--azure)]">{title}</h2>
                 <p className="text-xl">{text}</p>
             </div>
         </div>
