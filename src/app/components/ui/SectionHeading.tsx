09e7c057a396b13680f060599ec6d350d26b7c23
Jack <jackcmarshall@hotmail.com>
2026-06-01 19:04:28 +1000

    added md:text-size to heading for better scaling across devices

--- src/app/components/ui/SectionHeading.tsx
+++ src/app/components/ui/SectionHeading.tsx
@@ -5,6 +5,6 @@ type SectionHeadingProps = {
 
 export default function SectionHeading({ children, color }: SectionHeadingProps) {
     return (
-        <h1 className={`${color} text-5xl text-center font-bold mb-12`}>{ children }</h1>
+        <h1 className={`${color} text-4xl md:text-5xl text-center font-bold mb-12`}>{ children }</h1>
     );
 }
